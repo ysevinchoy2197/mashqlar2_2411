4d263e8553bdc534bc413d10c85cb5aad016b99c
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2025-11-24 15:39:42 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+#1-misol
+text = "salom dunyo  alik"
+print(f"text: {text}")
+soni = 0
+for i in range(len(text)):
+    if text[i] == ' ':
+        soni += 1
+
+print(f'Probellar soni: {soni}')
+
+#2-misol
+text = "Toshkentshahri"
+print(f"text: {text}")
+soni = 0
+for i in range(len(text)):
+    if text[i] == ' ':
+        soni += 1
+
+print(f'Probellar soni: {soni}')
+
+#3-misol
+text = "python  dasturlash  tili"
+print(f"text: {text}")
+soni = 0
+for i in range(len(text)):
+    if text[i] == ' ':
+        soni += 1
+
+print(f'Probellar soni: {soni}')
+
+#4-misol
+text = "salom dunyo!va alik?rahmat"
+print(f"text: {text}")
+soni = 0
+for i in range(len(text)):
+    if text[i] == ' ':
+        soni += 1
+
+print(f'Probellar soni: {soni}')
+
+#5-misol
+text = "a b c d e f g"
+print(f"text: {text}")
+soni = 0
+for i in range(len(text)):
+    if text[i] == ' ':
+        soni += 1
+
+print(f'Probellar soni: {soni}')
+
+#6-misol
+text = "salomdunyobugunyaxshi"
+print(f"text: {text}")
+soni = 0
+for i in range(len(text)):
+    if text[i] == ' ':
+        soni += 1
+
+print(f'Probellar soni: {soni}')
+
+#7-misol
+text = "  2025  yil  "
+print(f"text: {text}")
+soni = 0
+for i in range(len(text)):
+    if text[i] == ' ':
+        soni += 1
+
+print(f'Probellar soni: {soni}')
